66031b6bcb7bde5b62a381cecbaaef447f22c6e5
Nils ten Hoeve <nilstenhoeve@gmail.com>
2021-02-15 19:58:20 +0100

    Fixed #63

--- lib/annotation.dart
+++ lib/annotation.dart
@@ -1,7 +1,22 @@
+import 'formatting.dart';
+import 'parameter.dart';
+import 'basic.dart';
 import 'model.dart';
 
 class Annotation extends CodeModel {
+  final Type type;
+  final ParameterValues parameterValues;
+
+  Annotation(this.type,[this.parameterValues]);
+
   @override
-  // TODO: implement codes
-  List<CodeNode> codeNodes(Context context) => throw UnimplementedError();
+  List<CodeNode> codeNodes(Context context) => [
+    Code('@'),
+    type,
+    Code('('),
+    if (parameterValues != null) parameterValues,
+    Code(')'),
+    NewLine(),
+  ];
+
 }
---
+++ test/annotation_test.dart
@@ -0,0 +1,25 @@
+import 'package:dart_code/annotation.dart';
+import 'package:dart_code/basic.dart';
+import 'package:dart_code/expression.dart';
+import 'package:dart_code/parameter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  group('Annotation class', () {
+    test("Should return: '@Hidden()\n'", () {
+      String actual = Annotation(Type('Hidden')).toString();
+      String expected = '@Hidden()\n';
+      expect(actual, expected);
+    });
+
+    test("Should return: '@Hidden(forRole: \'admin\')\n'", () {
+      String actual = Annotation(
+          Type('Hidden'),
+          ParameterValues([
+            NamedParameterValue('forRole', Expression.ofString('admin'))
+          ])).toString();
+      String expected = '@Hidden(forRole: \'admin\')\n';
+      expect(actual, expected);
+    });
+  });
+}
